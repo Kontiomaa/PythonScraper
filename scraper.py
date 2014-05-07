4e6b3f391620cc280e26a058269171040f5d87f8
Samuel Kontiomaa <samuel.kontiomaa@gmail.com>
2014-05-07 22:58:49 +0300

    Scraper used at www.thl.fi to get data in a more computer friendly form

--- scraper.py
+++ scraper.py
@@ -3,19 +3,19 @@ from bs4 import BeautifulSoup
 import urllib2
 import csv
 
-#Insert url with tables below
-url='example.com'
+url='http://www.thl.fi/attachments/vanhuspalvelulainseuranta/ympvrk/2013/henkilostorakenne.html'
 soup = BeautifulSoup(urllib2.urlopen(url).read())
 
 f = open('datafile','w')
 
-tables = soup.findAll('table')
+tables = soup.findAll('table', attrs={'class': 'table'})
 for table in tables:
 	rows = table.findAll('tr')
 	for tr in rows:
 		cols = tr.findAll('td')
+		f.write('\n')
 		for td in cols:
-			#Insert correct charset below (current: cp1252=windows-1252)
+			#Insert correct charset below (cp1252=windows-1252)
 			f.write(td.text.encode('cp1252'))
 			f.write(';')
 f.close()
